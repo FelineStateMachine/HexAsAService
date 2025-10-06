1c2bd152c0554cdff0edea510afeed36c5122f67
dami <dami.etoile@gmail.com>
2025-10-05 22:16:04 -0600

    hexlinks v0.2

--- main.ts
+++ main.ts
@@ -7,13 +7,15 @@ const router = new Router();
 router.get("/", (ctx) => {
   ctx.response.body = `<!DOCTYPE html>
     <html>
-      <head><title>Ziplinks</title><head>
+      <head><title>Hexlinks</title><head>
       <body>
         <h1>Meowdy</h1>
-        <p>Create ziplinks easily to share links with friends.</p>
-        <p>Each link will be hexed! Cursed to shrink down to a vulnerable and balding 4-digit code.</p>
-        <p>Point at the puny urls and laugh!</p>
-        <!-- TODO: Forum with one text input for the url and a submit button to post the data to /new -->
+        <p>Hex your favorite link to easily share.</p>
+        <p>Each link will be cursed to shrink down to a vulnerable, and balding, 4-digit code.</p>
+        <form action="/new" method="post">
+          <input type="text" name="URL" placeholder="Enter URL" />
+          <button type="submit">Ziplink!</button>
+        </form>
       </body>
     </html>
   `;
@@ -22,46 +24,53 @@ router.get("/", (ctx) => {
 router.post("/new", async (ctx) => {
   if (!ctx.request.hasBody) {
     ctx.response.status = Status.BadRequest;
-    ctx.response.body = { error: "Request body is missing." };
+    ctx.response.body = { error: "Spell, Hex, requires a target." };
     return;
   }
-
-  const body = ctx.request.body({ type: "json" });
-  const data = await body.value;
-  console.debug("[Body]", data);
-  const longURL = data.URL;
+  const formData = await ctx.request.body.formData();
+  const longURL = formData.get("URL");
+  if (!URL.canParse(longURL)) {
+    console.error(`[UnprocessableEntity] ${ctx.request.ip} ${Date.now()}`)
+    ctx.response.status = Status.UnprocessableEntity;
+    ctx.response.body = {error: "The hex fizzles on non-urls."};
+    return;
+  }
+  
   const slug = crypto.randomUUID().substring(0, 4);
-  const result = await kv.set(["urls", slug], longURL);
-  console.log(`[New] ${slug.padStart(6)} ${Date.now()}`);
-  ctx.response.status = Status.Created;
+  await kv.set(["urls", slug], longURL);
+  
+  console.log(`[Created] ${slug.padStart(6)} ${Date.now()}`);
+  
   ctx.response.headers.set("Location", `/${slug}`);
-  ctx.response.body = { Destination: longURL };
+  ctx.response.status = Status.Created;
+  ctx.response.body = { URL: longURL, hex: slug };
 });
 
 router.get("/:slug", async (ctx) => {
   const slug = ctx.params.slug;
   if (slug.length !== 4) {
-    ctx.response.status = Status.BadRequest;
     console.error(`[BadRequest] ${ctx.request.ip} ${Date.now()}`);
+    ctx.response.status = Status.BadRequest;
+    ctx.response.body = {error: "You ain't gotta do all that. Hexes that big don't exist!"};
     return;
   }
   
-  console.info(`[Requested] ${slug.padStart(6)} ${Date.now()}`);
-  const entry = await kv.get(["urls", slug]);
-  console.debug(`[Result] ${entry} ${Date.now()}`);
+  console.log(`[Requested] ${slug.padStart(6)} ${Date.now()}`);
   
+  const entry = await kv.get(["urls", slug]);
   if (entry.value === null) {
     console.warn(`[NotFound] ${slug.padStart(6)} ${Date.now()}`);
     ctx.response.status = Status.NotFound;
+    ctx.response.body = {error: "Your URL is in another castle."};
     return;
   };
-  console.info(`[Found] ${slug.padStart(6)} ${Date.now()}`);
+  
+  console.log(`[Found] ${slug.padStart(6)} ${Date.now()}`);
   ctx.response.status = Status.MovedPermanently;
-  ctx.response.headers.set("Location", "https://www.youtube.com/");
+  ctx.response.headers.set("Location", entry.value as string);
 });
 
 const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
-
 app.listen({ port: 8080 });
