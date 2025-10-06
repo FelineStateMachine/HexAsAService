efa6aeb272d7efaa1bfb1bb281e7784824a8ef9e
dami <dami.etoile@gmail.com>
2025-10-05 21:19:01 -0600

    hexlinks v0.1

--- main.ts
+++ main.ts
@@ -1,5 +1,67 @@
-import { serveFile } from "jsr:@std/http/file-server";
+import { Application, Status } from "jsr:@oak/oak/";
+import { Router } from "jsr:@oak/oak/router";
 
-Deno.serve((req: Request) => {
-    return serveFile(req, "./index.html");
+const kv = await Deno.openKv();
+
+const router = new Router();
+router.get("/", (ctx) => {
+  ctx.response.body = `<!DOCTYPE html>
+    <html>
+      <head><title>Ziplinks</title><head>
+      <body>
+        <h1>Meowdy</h1>
+        <p>Create ziplinks easily to share links with friends.</p>
+        <p>Each link will be hexed! Cursed to shrink down to a vulnerable and balding 4-digit code.</p>
+        <p>Point at the puny urls and laugh!</p>
+        <!-- TODO: Forum with one text input for the url and a submit button to post the data to /new -->
+      </body>
+    </html>
+  `;
 });
+
+router.post("/new", async (ctx) => {
+  if (!ctx.request.hasBody) {
+    ctx.response.status = Status.BadRequest;
+    ctx.response.body = { error: "Request body is missing." };
+    return;
+  }
+
+  const body = ctx.request.body({ type: "json" });
+  const data = await body.value;
+  console.debug("[Body]", data);
+  const longURL = data.URL;
+  const slug = crypto.randomUUID().substring(0, 4);
+  const result = await kv.set(["urls", slug], longURL);
+  console.log(`[New] ${slug.padStart(6)} ${Date.now()}`);
+  ctx.response.status = Status.Created;
+  ctx.response.headers.set("Location", `/${slug}`);
+  ctx.response.body = { Destination: longURL };
+});
+
+router.get("/:slug", async (ctx) => {
+  const slug = ctx.params.slug;
+  if (slug.length !== 4) {
+    ctx.response.status = Status.BadRequest;
+    console.error(`[BadRequest] ${ctx.request.ip} ${Date.now()}`);
+    return;
+  }
+  
+  console.info(`[Requested] ${slug.padStart(6)} ${Date.now()}`);
+  const entry = await kv.get(["urls", slug]);
+  console.debug(`[Result] ${entry} ${Date.now()}`);
+  
+  if (entry.value === null) {
+    console.warn(`[NotFound] ${slug.padStart(6)} ${Date.now()}`);
+    ctx.response.status = Status.NotFound;
+    return;
+  };
+  console.info(`[Found] ${slug.padStart(6)} ${Date.now()}`);
+  ctx.response.status = Status.MovedPermanently;
+  ctx.response.headers.set("Location", "https://www.youtube.com/");
+});
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen({ port: 8080 });
